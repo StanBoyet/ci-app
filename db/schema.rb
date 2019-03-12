ccff98fa3969b89c619596cdeb70295b1560f920
Stanislas Boyet <stanislas@boyet.me>
2019-03-12 15:42:33 +0100

    merge schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_03_12_142740) do
+  enable_extension "plpgsql"
 
   create_table "segments", force: :cascade do |t|
     t.integer "value"
