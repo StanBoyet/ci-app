d0d8e7fcb00f5220c3e2e99b1414bd54481eeeb7
Stanislas Boyet <stanislas@boyet.me>
2019-03-12 19:25:49 +0100

    write tests

--- spec/models/segment_spec.rb
+++ spec/models/segment_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Segment, type: :model do
       segment = Segment.create(value: 2)
       expect(segment.round_trip).to eq(4)
     end
+
+    it 'can compute a double round way trip alongside it' do
+      value = 2
+      segment = Segment.create(value: value)
+      expect(segment.double_round_trip).to eq(value * 4)
+    end
   end
 end
